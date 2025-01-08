9298ac9f6d6b6ecaf31a528f69e8a73235df2d28
Ahmed Ibrahim <ahmedgouda797@gmail.com>
2025-01-07 12:28:32 -1200

    comments

--- server/services/openAi.js
+++ server/services/openAi.js
@@ -2,7 +2,12 @@ const OpenAI = require("openai");
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
-
+/**
+ * `getOpenAiResponse` Fetches a response from the OpenAI API.
+ * 
+ * @param {Array} messages - An array of all the previous messages and new ones to send to the API.
+ * @returns {String} - The response from the OpenAI API.
+ */
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
